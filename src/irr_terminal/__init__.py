5cbb17da5641eb91415c32bd4bc26fde163139b2
Raphael Azerad <azeradraphael10@gmail.com>
2026-06-04 14:57:55 -0500

    Add src package structure

---
+++ src/irr_terminal/__init__.py
@@ -0,0 +1,5 @@
+"""IRR Analytics Terminal public API."""
+
+from .calculations import FinancialMetrics, IRRAnalysis, analyze_irr, calculate_metrics
+
+__all__ = ["FinancialMetrics", "IRRAnalysis", "analyze_irr", "calculate_metrics"]
